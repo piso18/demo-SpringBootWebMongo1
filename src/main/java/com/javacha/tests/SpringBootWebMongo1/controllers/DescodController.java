f3e24461389a223116e457f9b160b889fd0bae91
javacha <javacha.ux@gmail.com>
2017-10-13 02:17:56 -0300

    Paginado

--- src/main/java/com/javacha/tests/SpringBootWebMongo1/controllers/DescodController.java
+++ src/main/java/com/javacha/tests/SpringBootWebMongo1/controllers/DescodController.java
@@ -3,9 +3,13 @@ package com.javacha.tests.SpringBootWebMongo1.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javacha.tests.SpringBootWebMongo1.beans.Descod;
@@ -32,21 +36,39 @@ public class DescodController {
         return descodRepository.findAllByTabla(tabla); 
     }	
 	
+	/**
+	 * Lista descod por nro de tabla con paginado. <br> Ejemplos<br>   http://localhost:8080//descod/1?page=2&size=10
+	 * <br> http://localhost:8080//descod/1?page=0&size=10&sort=descripcion,desc&sort=codigo
+	 * @param tabla
+	 * @return
+	 */
+    @RequestMapping(value = "{tabla}",  params = { "page" },  method = RequestMethod.GET)
+    public Page<Descod> listDescodByTablaPag(@PathVariable int tabla, Pageable pageable) {
+        return descodRepository.findAllByTabla(tabla, pageable); 
+    } 
+    
+    /*
+    @RequestMapping(value = "/{tabla}",  params = { "page"},  method = RequestMethod.GET)
+    public Page<Descod> listDescodByTablaPag(@PathVariable int tabla, @PageableDefault(size = 50) Pageable pageable) {
+        return descodRepository.findAllByTabla(tabla, pageable); 
+    }
+    */
+    
     /**
      * Obtiene codigo puntual de una tabla
      * @param tabla
      * @param codigo
      * @return
      */
-    /*
-    @RequestMapping(value = "/{tabla}/{codigo}",  method = RequestMethod.GET)
+   
+    @RequestMapping(value = "/{tabla}/codigo/{codigo}",  method = RequestMethod.GET)
     public Descod getDescodByTablaCodigo(@PathVariable int tabla, @PathVariable int codigo) {
         return descodRepository.findByTablaAndCodigo(tabla, codigo); 
     }	
-	 */
+
     
  
-    @RequestMapping(value = "/{tabla}/{desc}",  method = RequestMethod.GET)
+    @RequestMapping(value = "/{tabla}/descrip/{desc}",  method = RequestMethod.GET)
     public List<Descod> listDescodByTablaDescripcion(@PathVariable int tabla, @PathVariable String desc) {
         return descodRepository.findByTablaAndDescripcion(tabla, desc);
     }	
--- src/main/java/com/javacha/tests/SpringBootWebMongo1/repo/DescodRepository.java
+++ src/main/java/com/javacha/tests/SpringBootWebMongo1/repo/DescodRepository.java
@@ -1,18 +1,24 @@
 package com.javacha.tests.SpringBootWebMongo1.repo;
 
 import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
 import com.javacha.tests.SpringBootWebMongo1.beans.Descod;
 
-public interface DescodRepository  extends MongoRepository<Descod, Integer> {
+public interface DescodRepository  extends PagingAndSortingRepository<Descod, Integer> {
 
 		public List<Descod> findAllByTabla(Integer tabla);
+		public Page<Descod> findAllByTabla(Integer tabla, Pageable pageable);
+
 		public Descod findByTablaAndCodigo(Integer tabla, Integer codigo);
 	
 		
 		@Query("{ 'tabla' : ?0, 'descripcion':{$regex:?1,$options:'i'} }") 
 		List<Descod> findByTablaAndDescripcion(Integer tabla, String descripcion);
-		
-	
+					
 }
