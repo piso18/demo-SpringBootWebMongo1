251ff126ce7d870729ad9459d8a07ea46fd78b17
javacha <javacha@gmail.com>
2017-10-22 20:12:53 -0300

    Se agrega soporte Cors

--- src/main/java/com/javacha/tests/SpringBootWebMongo1/CorsConfiguration.java
+++ src/main/java/com/javacha/tests/SpringBootWebMongo1/CorsConfiguration.java
@@ -1,32 +1,25 @@
 package com.javacha.tests.SpringBootWebMongo1;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@SpringBootApplication
-public class SpringBootWebMongo1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootWebMongo1Application.class, args);
-	}
-	
-	
-	/*
+@Configuration
+public class CorsConfiguration {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-            	System.out.println("---configuro cors---");
+            	System.out.println("--cors---");
                 registry.addMapping("/**");
             }
         };
     }
-    */	
-    
 }
+		
--- src/main/java/com/javacha/tests/SpringBootWebMongo1/controllers/DescodController.java
+++ src/main/java/com/javacha/tests/SpringBootWebMongo1/controllers/DescodController.java
@@ -1,15 +1,14 @@
 package com.javacha.tests.SpringBootWebMongo1.controllers;
+  
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javacha.tests.SpringBootWebMongo1.beans.Descod;
